1747a937de9cf6e8f3c9f58a9c8fac844f733654
cdz <zhouwench@gmail.com>
2016-05-04 23:07:59 +0800

    update

--- ktransfer/ktransfer/module.go
+++ ktransfer/ktransfer/module.go
@@ -1,18 +1,18 @@
 /*
     The MIT License (MIT)
-    
+
 	Copyright (c) 2015 myhug.cn and zhouwench (zhouwench@gmail.com)
-    
+
     Permission is hereby granted, free of charge, to any person obtaining a copy
     of this software and associated documentation files (the "Software"), to deal
     in the Software without restriction, including without limitation the rights
     to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
     copies of the Software, and to permit persons to whom the Software is
     furnished to do so, subject to the following conditions:
-    
+
     The above copyright notice and this permission notice shall be included in all
     copies or substantial portions of the Software.
-    
+
     THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
     IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
     FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -24,34 +24,34 @@
 package ktransfer
 
 import (
-		"github.com/dzch/go-utils/logger"
-		"github.com/go-yaml/yaml"
-		"fmt"
-		"io/ioutil"
-		"errors"
-		"time"
-	   )
+	"errors"
+	"fmt"
+	"github.com/dzch/go-utils/logger"
+	"gopkg.in/yaml.v2"
+	"io/ioutil"
+	"time"
+)
 
 type Module struct {
-	name string
-	zkHosts []string
-	zkChroot string
-	zkTimeout time.Duration
-	moduleConfDir string
-	moduleConfig map[interface{}]interface{}
-	topic string
-	methodEnabled map[string]bool
-	protocolConfig map[interface{}]interface{}
-	workerNum uint32
-	maxRetryTimes int    // -1, 无限重试
+	name              string
+	zkHosts           []string
+	zkChroot          string
+	zkTimeout         time.Duration
+	moduleConfDir     string
+	moduleConfig      map[interface{}]interface{}
+	topic             string
+	methodEnabled     map[string]bool
+	protocolConfig    map[interface{}]interface{}
+	workerNum         uint32
+	maxRetryTimes     int // -1, 无限重试
 	failRetryInterval time.Duration
-	windowSize int
-	backendServers []string
-	transDi *TransDi
-	fatalErrorChan chan error
+	windowSize        int
+	backendServers    []string
+	transDi           *TransDi
+	fatalErrorChan    chan error
 }
 
-func (m *Module) init () (err error) {
+func (m *Module) init() (err error) {
 	err = m.initConfig()
 	if err != nil {
 		return
@@ -65,18 +65,18 @@ func (m *Module) init () (err error) {
 
 func (m *Module) run() {
 	logger.Notice("module [%s] start", m.name)
-    m.transDi.run()
+	m.transDi.run()
 	// TODO: delete it later
 	m.fatalErrorChan <- errors.New("no error")
 }
 
 func (m *Module) initConfig() (err error) {
-    content, err := ioutil.ReadFile(fmt.Sprintf("%s/%s.yaml", m.moduleConfDir, m.name))
+	content, err := ioutil.ReadFile(fmt.Sprintf("%s/%s.yaml", m.moduleConfDir, m.name))
 	if err != nil {
 		logger.Warning("fail to read module config: %s", err.Error())
 		return
 	}
-    mc := make(map[interface{}]interface{})
+	mc := make(map[interface{}]interface{})
 	err = yaml.Unmarshal(content, &mc)
 	if err != nil {
 		return
@@ -84,21 +84,21 @@ func (m *Module) initConfig() (err error) {
 	m.moduleConfig = mc
 
 	/* topic conf */
-    topic, ok := mc["topic"]
+	topic, ok := mc["topic"]
 	if !ok {
 		err = errors.New(fmt.Sprintf("topic not found in module conf file, module [%s]", m.name))
 		logger.Warning("%s", err.Error())
 		return
-    }
+	}
 	m.topic = topic.(string)
 
 	/* method conf */
 	m.methodEnabled = make(map[string]bool)
 	methods, ok := mc["methods"]
 	if ok {
-	    for _, method := range methods.([]interface{}) {
-		    m.methodEnabled[method.(string)] = true
-	    }
+		for _, method := range methods.([]interface{}) {
+			m.methodEnabled[method.(string)] = true
+		}
 	}
 
 	/* protocol conf */
@@ -107,7 +107,7 @@ func (m *Module) initConfig() (err error) {
 		err = errors.New(fmt.Sprintf("protocol not found in module conf file, module [%s]", m.name))
 		logger.Warning("%s", err.Error())
 		return
-    }
+	}
 	m.protocolConfig = protocol.(map[interface{}]interface{})
 
 	/* worker conf */
@@ -116,7 +116,7 @@ func (m *Module) initConfig() (err error) {
 		err = errors.New(fmt.Sprintf("worker_num not found in module conf file, module [%s]", m.name))
 		logger.Warning("%s", err.Error())
 		return
-    }
+	}
 	m.workerNum = uint32(workerNum.(int))
 	maxRetryTimes, ok := mc["max_retry_times"]
 	if !ok {
@@ -131,7 +131,7 @@ func (m *Module) initConfig() (err error) {
 		logger.Warning("%s", err.Error())
 		return
 	}
-	m.failRetryInterval = time.Duration(failRetryInterval.(int))*time.Millisecond
+	m.failRetryInterval = time.Duration(failRetryInterval.(int)) * time.Millisecond
 
 	/* window size */
 	windowSize, ok := mc["window_size"]
@@ -139,7 +139,7 @@ func (m *Module) initConfig() (err error) {
 		err = errors.New(fmt.Sprintf("window_size not found in module conf file, module [%s]", m.name))
 		logger.Warning("%s", err.Error())
 		return
-    }
+	}
 	m.windowSize = windowSize.(int)
 
 	/* backend servers */
@@ -157,20 +157,20 @@ func (m *Module) initConfig() (err error) {
 }
 
 func (m *Module) initTransDi() (err error) {
-	m.transDi = &TransDi {
-        moduleName: m.name,
-		zkHosts: m.zkHosts,
-		zkChroot: m.zkChroot,
-		zkTimeout: m.zkTimeout,
-		backendServers: m.backendServers,
-		protocolConfig: m.protocolConfig,
-		windowSize: m.windowSize,
-		workerNum: m.workerNum,
-		fatalErrorChan: m.fatalErrorChan,
-		topic: m.topic,
-		methodEnabled: m.methodEnabled,
-	    maxRetryTimes: m.maxRetryTimes,
-	    failRetryInterval: m.failRetryInterval,
+	m.transDi = &TransDi{
+		moduleName:        m.name,
+		zkHosts:           m.zkHosts,
+		zkChroot:          m.zkChroot,
+		zkTimeout:         m.zkTimeout,
+		backendServers:    m.backendServers,
+		protocolConfig:    m.protocolConfig,
+		windowSize:        m.windowSize,
+		workerNum:         m.workerNum,
+		fatalErrorChan:    m.fatalErrorChan,
+		topic:             m.topic,
+		methodEnabled:     m.methodEnabled,
+		maxRetryTimes:     m.maxRetryTimes,
+		failRetryInterval: m.failRetryInterval,
 	}
 	return m.transDi.init()
 }
